c2f8977aed9c4587de58b71953f4ae61387d89ad
Nikol <nikolam1997@gmail.com>
2019-02-01 15:49:30 -0400

    Nikollll

--- Logan.java
+++ Logan.java
@@ -2,5 +2,7 @@ public class Logan{
 
      public static void main(String[] args){
      system.out.println("Logan Shane Fitzpatrick");
+     System.out.println("Hey buddy");
+     System.out.println("special tricks of dogs sniffing bees that are sick");
      }
 }
